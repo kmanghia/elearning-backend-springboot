9fa25b59439ade337d8c1f6021d27fc84b79d797
kmanghia <lam47788@gmail.com>
2026-01-17 18:02:28 +0700

    fix: remove Lombok from JwtAuthenticationResponse

--- Vanca/src/main/java/com/example/demo/dto/response/JwtAuthenticationResponse.java
+++ Vanca/src/main/java/com/example/demo/dto/response/JwtAuthenticationResponse.java
@@ -1,15 +1,60 @@
 package com.example.demo.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class JwtAuthenticationResponse {
 	private String accessToken;
 	private String tokenType = "Bearer";
 	private Long userId;
 	private String email;
 	private String role;
-}
 
+	public JwtAuthenticationResponse() {
+	}
+
+	public JwtAuthenticationResponse(String accessToken, String tokenType, Long userId, String email, String role) {
+		this.accessToken = accessToken;
+		this.tokenType = tokenType;
+		this.userId = userId;
+		this.email = email;
+		this.role = role;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
