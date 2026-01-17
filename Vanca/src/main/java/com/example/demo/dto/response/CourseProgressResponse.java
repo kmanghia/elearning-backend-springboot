908baa8f34c6816f8a1e7b767a7a1c2e2748df26
kmanghia <lam47788@gmail.com>
2026-01-17 18:02:02 +0700

    fix: remove Lombok from CourseProgressResponse

--- Vanca/src/main/java/com/example/demo/dto/response/CourseProgressResponse.java
+++ Vanca/src/main/java/com/example/demo/dto/response/CourseProgressResponse.java
@@ -1,17 +1,7 @@
 package com.example.demo.dto.response;
 
-import com.example.demo.model.Progress;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class CourseProgressResponse {
 	private Long courseId;
 	private Integer totalLessons;
@@ -19,15 +9,95 @@ public class CourseProgressResponse {
 	private Integer courseProgress; // Percentage (0-100)
 	private List<LessonProgressResponse> lessonProgress;
 
-	@Data
-	@Builder
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class LessonProgressResponse {
-		private Long lessonId;
-		private String lessonTitle;
-		private Integer watchedDurationSeconds;
-		private Integer completionPercentage;
-		private Boolean isCompleted;
+	public CourseProgressResponse() {
 	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder {
+		private Long courseId;
+		private Integer totalLessons;
+		private Integer completedLessons;
+		private Integer courseProgress;
+		private List<LessonProgressResponse> lessonProgress;
+
+		public Builder courseId(Long courseId) {
+			this.courseId = courseId;
+			return this;
+		}
+
+		public Builder totalLessons(Integer totalLessons) {
+			this.totalLessons = totalLessons;
+			return this;
+		}
+
+		public Builder completedLessons(Integer completedLessons) {
+			this.completedLessons = completedLessons;
+			return this;
+		}
+
+		public Builder courseProgress(Integer courseProgress) {
+			this.courseProgress = courseProgress;
+			return this;
+		}
+
+		public Builder lessonProgress(List<LessonProgressResponse> lessonProgress) {
+			this.lessonProgress = lessonProgress;
+			return this;
+		}
+
+		public CourseProgressResponse build() {
+			CourseProgressResponse response = new CourseProgressResponse();
+			response.setCourseId(courseId);
+			response.setTotalLessons(totalLessons);
+			response.setCompletedLessons(completedLessons);
+			response.setCourseProgress(courseProgress);
+			response.setLessonProgress(lessonProgress);
+			return response;
+		}
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public Integer getTotalLessons() {
+		return totalLessons;
+	}
+
+	public void setTotalLessons(Integer totalLessons) {
+		this.totalLessons = totalLessons;
+	}
+
+	public Integer getCompletedLessons() {
+		return completedLessons;
+	}
+
+	public void setCompletedLessons(Integer completedLessons) {
+		this.completedLessons = completedLessons;
+	}
+
+	public Integer getCourseProgress() {
+		return courseProgress;
+	}
+
+	public void setCourseProgress(Integer courseProgress) {
+		this.courseProgress = courseProgress;
+	}
+
+	public List<LessonProgressResponse> getLessonProgress() {
+		return lessonProgress;
+	}
+
+	public void setLessonProgress(List<LessonProgressResponse> lessonProgress) {
+		this.lessonProgress = lessonProgress;
+	}
+
+	// Nested class LessonProgressResponse removed - using standalone class instead
 }
