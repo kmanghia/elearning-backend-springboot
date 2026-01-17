4833e7e76f9c26affe481240857588475b104e11
kmanghia <lam47788@gmail.com>
2026-01-17 18:05:51 +0700

    fix: remove Lombok from QuestionOption model

--- Vanca/src/main/java/com/example/demo/model/QuestionOption.java
+++ Vanca/src/main/java/com/example/demo/model/QuestionOption.java
@@ -1,15 +1,9 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "question_options")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class QuestionOption {
 
 	@Id
@@ -28,5 +22,57 @@ public class QuestionOption {
 
 	@Column(name = "order_index", nullable = false)
 	private Integer orderIndex;
-}
 
+	// Constructors
+	public QuestionOption() {
+	}
+
+	public QuestionOption(Long id, Question question, String content, Boolean isCorrect, Integer orderIndex) {
+		this.id = id;
+		this.question = question;
+		this.content = content;
+		this.isCorrect = isCorrect;
+		this.orderIndex = orderIndex;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Boolean getIsCorrect() {
+		return isCorrect;
+	}
+
+	public void setIsCorrect(Boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+
+	public Integer getOrderIndex() {
+		return orderIndex;
+	}
+
+	public void setOrderIndex(Integer orderIndex) {
+		this.orderIndex = orderIndex;
+	}
+}
