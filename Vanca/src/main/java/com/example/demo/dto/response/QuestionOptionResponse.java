fc68e5c11f1bc8c193e239c7f9b418af110fae38
kmanghia <lam47788@gmail.com>
2026-01-17 18:02:50 +0700

    fix: remove Lombok from QuestionOptionResponse

--- Vanca/src/main/java/com/example/demo/dto/response/QuestionOptionResponse.java
+++ Vanca/src/main/java/com/example/demo/dto/response/QuestionOptionResponse.java
@@ -1,18 +1,68 @@
 package com.example.demo.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class QuestionOptionResponse {
 	private Long id;
 	private String content;
 	private Integer orderIndex;
 	// Note: isCorrect is not included to prevent cheating
-}
 
+	public QuestionOptionResponse() {
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder {
+		private Long id;
+		private String content;
+		private Integer orderIndex;
+
+		public Builder id(Long id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder content(String content) {
+			this.content = content;
+			return this;
+		}
+
+		public Builder orderIndex(Integer orderIndex) {
+			this.orderIndex = orderIndex;
+			return this;
+		}
+
+		public QuestionOptionResponse build() {
+			QuestionOptionResponse response = new QuestionOptionResponse();
+			response.setId(id);
+			response.setContent(content);
+			response.setOrderIndex(orderIndex);
+			return response;
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getOrderIndex() {
+		return orderIndex;
+	}
+
+	public void setOrderIndex(Integer orderIndex) {
+		this.orderIndex = orderIndex;
+	}
+}
