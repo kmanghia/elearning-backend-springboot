69c1dbff47da4e0ceb688017f8ffdeac1a2c2de9
kmanghia <lam47788@gmail.com>
2026-01-17 17:58:50 +0700

    fix: remove Lombok from CreateCertificateRequest

--- Vanca/src/main/java/com/example/demo/dto/request/CreateCertificateRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/CreateCertificateRequest.java
@@ -1,18 +1,38 @@
 package com.example.demo.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateCertificateRequest {
-	
+
 	@NotNull(message = "Course ID is required")
 	private Long courseId;
-	
+
 	@NotNull(message = "Student ID is required")
 	private Long studentId;
+
+	// Constructors
+	public CreateCertificateRequest() {
+	}
+
+	public CreateCertificateRequest(Long courseId, Long studentId) {
+		this.courseId = courseId;
+		this.studentId = studentId;
+	}
+
+	// Getters and Setters
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
 }
