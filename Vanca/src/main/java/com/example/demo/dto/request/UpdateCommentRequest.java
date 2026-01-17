106334a514534f15725944475b63adf2b6630f20
kmanghia <lam47788@gmail.com>
2026-01-17 18:00:45 +0700

    fix: remove Lombok from UpdateCommentRequest

--- Vanca/src/main/java/com/example/demo/dto/request/UpdateCommentRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/UpdateCommentRequest.java
@@ -1,15 +1,26 @@
 package com.example.demo.dto.request;
 
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateCommentRequest {
-	
+
 	@Size(min = 5, message = "Content must be at least 5 characters")
 	private String content;
+
+	// Constructors
+	public UpdateCommentRequest() {
+	}
+
+	public UpdateCommentRequest(String content) {
+		this.content = content;
+	}
+
+	// Getters and Setters
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
