e59eab369173074fa28b6ff1200a97eda0976ed3
kmanghia <lam47788@gmail.com>
2026-01-17 18:08:33 +0700

    fix: remove Lombok from AuthController

--- Vanca/src/main/java/com/example/demo/controller/AuthController.java
+++ Vanca/src/main/java/com/example/demo/controller/AuthController.java
@@ -5,16 +5,19 @@ import com.example.demo.dto.request.RegisterRequest;
 import com.example.demo.dto.response.JwtAuthenticationResponse;
 import com.example.demo.service.AuthService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@RequiredArgsConstructor
 public class AuthController {
 
 	private final AuthService authService;
+
+
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
 
 	@PostMapping("/register")
 	public ResponseEntity<JwtAuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
