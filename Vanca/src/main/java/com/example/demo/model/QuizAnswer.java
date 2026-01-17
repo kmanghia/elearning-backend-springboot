8f634e207a92675d97fc8ba556f19d60da4edea8
kmanghia <lam47788@gmail.com>
2026-01-17 18:05:57 +0700

    fix: remove Lombok from QuizAnswer model

--- Vanca/src/main/java/com/example/demo/model/QuizAnswer.java
+++ Vanca/src/main/java/com/example/demo/model/QuizAnswer.java
@@ -1,18 +1,12 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "quiz_answers")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class QuizAnswer {
 
 	@Id
@@ -28,14 +22,63 @@ public class QuizAnswer {
 	private Question question;
 
 	@ManyToMany
-	@JoinTable(
-		name = "answer_selected_options",
-		joinColumns = @JoinColumn(name = "answer_id"),
-		inverseJoinColumns = @JoinColumn(name = "option_id")
-	)
+	@JoinTable(name = "answer_selected_options", joinColumns = @JoinColumn(name = "answer_id"), inverseJoinColumns = @JoinColumn(name = "option_id"))
 	private Set<QuestionOption> selectedOptions = new HashSet<>();
 
 	@Column(name = "is_correct", nullable = false)
 	private Boolean isCorrect = false;
-}
 
+	// Constructors
+	public QuizAnswer() {
+	}
+
+	public QuizAnswer(Long id, QuizAttempt attempt, Question question, Set<QuestionOption> selectedOptions,
+			Boolean isCorrect) {
+		this.id = id;
+		this.attempt = attempt;
+		this.question = question;
+		this.selectedOptions = selectedOptions;
+		this.isCorrect = isCorrect;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public QuizAttempt getAttempt() {
+		return attempt;
+	}
+
+	public void setAttempt(QuizAttempt attempt) {
+		this.attempt = attempt;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public Set<QuestionOption> getSelectedOptions() {
+		return selectedOptions;
+	}
+
+	public void setSelectedOptions(Set<QuestionOption> selectedOptions) {
+		this.selectedOptions = selectedOptions;
+	}
+
+	public Boolean getIsCorrect() {
+		return isCorrect;
+	}
+
+	public void setIsCorrect(Boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+}
