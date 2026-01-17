87b0baa749d6862ee17321d756270ca3c5577f1f
kmanghia <lam47788@gmail.com>
2026-01-17 18:05:48 +0700

    fix: remove Lombok from Question model

--- Vanca/src/main/java/com/example/demo/model/Question.java
+++ Vanca/src/main/java/com/example/demo/model/Question.java
@@ -1,18 +1,12 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "questions")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Question {
 
 	@Id
@@ -41,8 +35,79 @@ public class Question {
 	@OrderBy("orderIndex ASC")
 	private List<QuestionOption> options = new ArrayList<>();
 
+	// Constructors
+	public Question() {
+	}
+
+	public Question(Long id, Quiz quiz, String content, QuestionType questionType, Integer points,
+			Integer orderIndex, List<QuestionOption> options) {
+		this.id = id;
+		this.quiz = quiz;
+		this.content = content;
+		this.questionType = questionType;
+		this.points = points;
+		this.orderIndex = orderIndex;
+		this.options = options;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Quiz getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(Quiz quiz) {
+		this.quiz = quiz;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public QuestionType getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(QuestionType questionType) {
+		this.questionType = questionType;
+	}
+
+	public Integer getPoints() {
+		return points;
+	}
+
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+
+	public Integer getOrderIndex() {
+		return orderIndex;
+	}
+
+	public void setOrderIndex(Integer orderIndex) {
+		this.orderIndex = orderIndex;
+	}
+
+	public List<QuestionOption> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<QuestionOption> options) {
+		this.options = options;
+	}
+
 	public enum QuestionType {
 		SINGLE_CHOICE, MULTIPLE_CHOICE, TRUE_FALSE
 	}
 }
-
