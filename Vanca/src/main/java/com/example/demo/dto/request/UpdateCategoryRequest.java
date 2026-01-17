42448f7f28165662d7851c18634c57739267ba94
kmanghia <lam47788@gmail.com>
2026-01-17 18:00:42 +0700

    fix: remove Lombok from UpdateCategoryRequest

--- Vanca/src/main/java/com/example/demo/dto/request/UpdateCategoryRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/UpdateCategoryRequest.java
@@ -1,17 +1,45 @@
 package com.example.demo.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateCategoryRequest {
-	
+
 	private String name;
-	
+
 	private String description;
-	
+
 	private String iconUrl;
+
+	// Constructors
+	public UpdateCategoryRequest() {
+	}
+
+	public UpdateCategoryRequest(String name, String description, String iconUrl) {
+		this.name = name;
+		this.description = description;
+		this.iconUrl = iconUrl;
+	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIconUrl() {
+		return iconUrl;
+	}
+
+	public void setIconUrl(String iconUrl) {
+		this.iconUrl = iconUrl;
+	}
 }
