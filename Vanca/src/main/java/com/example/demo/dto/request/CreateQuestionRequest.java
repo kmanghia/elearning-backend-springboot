e3da7fda0a2437a9f27c3fd3d0001d5efd587d62
kmanghia <lam47788@gmail.com>
2026-01-17 17:59:49 +0700

    fix: remove Lombok from CreateQuestionRequest

--- Vanca/src/main/java/com/example/demo/dto/request/CreateQuestionRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/CreateQuestionRequest.java
@@ -6,12 +6,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 public class CreateQuestionRequest {
 	@NotBlank(message = "Question content is required")
 	private String content;
@@ -27,4 +25,58 @@ public class CreateQuestionRequest {
 
 	@Valid
 	private List<CreateQuestionOptionRequest> options = new ArrayList<>();
+
+	// Constructors
+	public CreateQuestionRequest() {
+	}
+
+	public CreateQuestionRequest(String content, Question.QuestionType questionType, Integer points,
+			Integer orderIndex, List<CreateQuestionOptionRequest> options) {
+		this.content = content;
+		this.questionType = questionType;
+		this.points = points;
+		this.orderIndex = orderIndex;
+		this.options = options;
+	}
+
+	// Getters and Setters
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Question.QuestionType getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(Question.QuestionType questionType) {
+		this.questionType = questionType;
+	}
+
+	public Integer getPoints() {
+		return points;
+	}
+
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+
+	public Integer getOrderIndex() {
+		return orderIndex;
+	}
+
+	public void setOrderIndex(Integer orderIndex) {
+		this.orderIndex = orderIndex;
+	}
+
+	public List<CreateQuestionOptionRequest> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<CreateQuestionOptionRequest> options) {
+		this.options = options;
+	}
 }
