e5827e8308bf4498fbe05bc790b687b2f4515cb0
kmanghia <lam47788@gmail.com>
2026-01-17 18:07:48 +0700

    feat: add CreatePaymentIntentRequest DTO

---
+++ Vanca/src/main/java/com/example/demo/dto/request/CreatePaymentIntentRequest.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto.request;
+
+public class CreatePaymentIntentRequest {
+	// Optional metadata for payment intent
+	// Can be empty body
+
+	// Constructors
+	public CreatePaymentIntentRequest() {
+	}
+}
