1e1cce772653a4033ed5c950cb8171a6c04aa6c5
kmanghia <lam47788@gmail.com>
2026-01-17 18:00:57 +0700

    fix: remove Lombok from UpdateProgressRequest

--- Vanca/src/main/java/com/example/demo/dto/request/UpdateProgressRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/UpdateProgressRequest.java
@@ -2,12 +2,26 @@ package com.example.demo.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.Data;
 
-@Data
 public class UpdateProgressRequest {
 	@NotNull(message = "Watched duration is required")
 	@PositiveOrZero(message = "Watched duration must be positive or zero")
 	private Integer watchedDurationSeconds;
-}
 
+	// Constructors
+	public UpdateProgressRequest() {
+	}
+
+	public UpdateProgressRequest(Integer watchedDurationSeconds) {
+		this.watchedDurationSeconds = watchedDurationSeconds;
+	}
+
+	// Getters and Setters
+	public Integer getWatchedDurationSeconds() {
+		return watchedDurationSeconds;
+	}
+
+	public void setWatchedDurationSeconds(Integer watchedDurationSeconds) {
+		this.watchedDurationSeconds = watchedDurationSeconds;
+	}
+}
