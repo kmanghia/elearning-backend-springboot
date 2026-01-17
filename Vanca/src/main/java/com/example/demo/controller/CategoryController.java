f1e85370731983a5ef119722dd7f76a1755db85d
kmanghia <lam47788@gmail.com>
2026-01-17 17:57:32 +0700

    fix: remove Lombok from CategoryController

--- Vanca/src/main/java/com/example/demo/controller/CategoryController.java
+++ Vanca/src/main/java/com/example/demo/controller/CategoryController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,11 +17,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@RequiredArgsConstructor
 @Tag(name = "Category", description = "Course category management APIs")
 public class CategoryController {
 	
 	private final CategoryService categoryService;
+
+
+	public CategoryController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
 	
 	@PostMapping
 	@PreAuthorize("hasRole('ADMIN')")
