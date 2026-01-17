49a30021faaa608931e57ff5b3b374f974a6aafe
kmanghia <lam47788@gmail.com>
2026-01-17 18:00:28 +0700

    fix: remove Lombok from CreateVoteRequest

--- Vanca/src/main/java/com/example/demo/dto/request/CreateVoteRequest.java
+++ Vanca/src/main/java/com/example/demo/dto/request/CreateVoteRequest.java
@@ -2,18 +2,47 @@ package com.example.demo.dto.request;
 
 import com.example.demo.model.Vote;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateVoteRequest {
-	
+
 	private Long postId;
 	private Long commentId;
-	
+
 	@NotNull(message = "Vote type is required")
 	private Vote.VoteType voteType;
+
+	// Constructors
+	public CreateVoteRequest() {
+	}
+
+	public CreateVoteRequest(Long postId, Long commentId, Vote.VoteType voteType) {
+		this.postId = postId;
+		this.commentId = commentId;
+		this.voteType = voteType;
+	}
+
+	// Getters and Setters
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public Long getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Long commentId) {
+		this.commentId = commentId;
+	}
+
+	public Vote.VoteType getVoteType() {
+		return voteType;
+	}
+
+	public void setVoteType(Vote.VoteType voteType) {
+		this.voteType = voteType;
+	}
 }
